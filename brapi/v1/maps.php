567dbad615a8c23132afbec5141bb39ba5a6993e
Clay Birkett <clb343@cornell.edu>
2018-02-09 09:15:17 -0800

    BrAPI - fix format of maps call

--- brapi/v1/maps.php
+++ brapi/v1/maps.php
@@ -2,7 +2,6 @@
 /**
  * brapi/v1/maps.php
  * Deliver genome maps according to http://docs.brapi.apiary.io
- *
  */
 
 require '../../includes/bootstrap.inc';
@@ -157,7 +156,8 @@ if ($action == "list") {
     $return = json_encode($linearray);
     echo "$return";
 } elseif ($action == "MapData") {
-    $linearray['metadata']['status'] = null;
+    $linearray['metadata']['status'] = array();
+    $linearray['metadata']['datafiles'] = array();
     $num_rows = 0;
     if (empty($uid_ary)) {
         $sql = "select markers.marker_uid, markers.marker_name, start_position, chromosome, arm
