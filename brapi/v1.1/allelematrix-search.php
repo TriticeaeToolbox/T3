13f50ff0142d35438d8aa2e82606e043b757dba0
Clay Birkett <clb343@cornell.edu>
2018-02-22 06:07:17 -0800

    BrAPI - fix CORS for POST

---
+++ brapi/v1.1/allelematrix-search.php
@@ -0,0 +1,252 @@
+<?php
+require '../../includes/bootstrap.inc';
+$mysqli = connecti();
+
+$self = $_SERVER['PHP_SELF'];
+$script = $_SERVER["SCRIPT_NAME"]."/";
+$rest = str_replace($script, "", $self);
+$rest = explode("/", $rest);
+
+$results['metadata']['status'] = array();
+$results['metadata']['datafiles'] = array();
+
+if (isset($_REQUEST['pageSize'])) {
+    $pageSize = $_REQUEST['pageSize'];
+} else {
+    $pageSize = 1000;
+}
+if (isset($_REQUEST['page'])) {
+    $currentPage = $_REQUEST['page'];
+} else {
+    $currentPage = 0;
+}
+if (isset($_REQUEST['format'])) {
+    $format = $_REQUEST['format'];
+} else {
+    $format = "json";
+}
+$logFile = "/tmp/tht/request-log.txt";
+
+$profile_list = array();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $fh = fopen($logFile, "w");
+    fwrite($fh, "POST $format\n");
+    $request = json_decode(file_get_contents('php://input'), true);
+    foreach ($request as $key => $val) {
+        fwrite($fh, "key=$key\nval=$val\n");
+        if ($key == "markerprofileDbId") {
+            $profile_str = implode(",", $val);
+            $profile_list = $val;
+        } elseif ($key == "format") {
+            $format = $val;
+        } elseif ($key == "page") {
+            $currentPage = $val;
+        } elseif ($key == "pageSize") {
+            $pageSize = $val;
+        }
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $fh = fopen($logFile, "a");
+    fwrite($fh, "GET $format\n");
+    foreach ($_GET as $key => $val) {
+        fwrite($fh, "key=$key val=$val\n");
+        if ($key == "markerprofileDbId") {
+            $profile_str = $val;
+            $profile_list = explode(",", $match[1]);
+        }
+    }
+    fwrite($fh, "$rest[0] $rest[1] $rest[2]\n");
+} elseif ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    header("Content-Type: application/json");
+    die();
+} else {
+    dieNice("Error", "invalid request method");
+}
+
+function dieNice($code, $msg)
+{
+    global $results;
+    $results['metadata']['pagination'] = null;
+    $results['metadata']['status'][] = array("code" => $code, "message" => "$msg");
+    $results['result']['data'] = array();
+    $return = json_encode($results);
+    header("Content-Type: application/json");
+    die("$return");
+}
+
+if ($rest[0] == "status") {
+    $unqStr = $rest[1];
+    $results['metadata']['pagination'] = null;
+    $results['result'] = null;
+    $base_url = "https://" . $_SERVER['HTTP_HOST'];
+    $tmpFile = $base_url . "/tmp/tht/download_" . $unqStr . ".tsv";
+    $statusFile = "/tmp/tht/status_" . $unqStr . ".txt";
+    $results['metadata']['datafiles'] = array($tmpFile);
+    if (file_exists($statusFile)) {
+        if (filesize($statusFile) > 0) {
+            $results['metadata']['status'][] = array("code" => "asyncstatus", "message" => "FAILED");
+        } else {
+            $results['metadata']['status'][] = array("code" => "asyncstatus", "message" => "FINISHED");
+        }
+    } else {
+        $results['metadata']['status'][] = array("code" => "asyncstatus", "message" => "INPROCESS");
+    }
+    $return = json_encode($results);
+    fwrite($fh, $return);
+    header("Content-Type: application/json");
+    die("$return");
+} elseif ($profile_list != array()) {
+    $uniqueStr = chr(rand(65, 80)).chr(rand(65, 80)).chr(rand(65, 80)).chr(rand(65, 80));
+    $errorFile = "/tmp/tht/error_" . $uniqueStr . ".txt";
+
+    //first query all data
+    foreach ($profile_list as $item) {
+        if (preg_match("/(\d+)_(\d+)/", $item, $match)) {
+            $exp_ary[] = $match[2];
+        } else {
+            $results['metadata']['status'][] = array("code" => "parm", "message" => "Error: invalid format of marker profile id $item");
+            continue;
+        }
+    }
+
+    $countExp = count($profile_list);
+    if ($format != "json") {
+        $cmd = "php allelematrix-search-offline.php $profile_str $uniqueStr $format > /dev/null 2> $errorFile";
+        fwrite($fh, "$cmd\n");
+        exec($cmd);
+        dieNice("asynchid", "$uniqueStr");
+    }
+    $num_rows = 0;
+    foreach ($profile_list as $item) {
+        //echo "profile = $item\n";
+        if (preg_match("/(\d+)_(\d+)/", $item, $match)) {
+            $lineuid = $match[1];
+            $expid = $match[2];
+        } else {
+            dieNice("Error", "invalid format of marker profile id $item");
+        }
+
+        //get marker_uid
+        $sql = "select marker_index from allele_byline_expidx where experiment_uid = $expid";
+        $res = mysqli_query($mysqli, $sql);
+        if ($row = mysqli_fetch_row($res)) {
+            $marker_index = $row[0];
+            $marker_index = json_decode($marker_index, true);
+            //$marker_index = explode(",", $marker_index);
+        } else {
+            dieNice("Error", "invalid experiment $expid");
+        }
+
+        //now get just those selected
+        $sql = "select alleles from allele_byline_exp_ACTG where experiment_uid = $expid and line_record_uid = $lineuid";
+        if ($currentPage == 0) {
+        } else {
+            $offset = $currentPage * $pageSize;
+            if ($offset < 0) {
+                $offset = 0;
+            }
+        }
+        $found = 0;
+        if ($res = mysqli_query($mysqli, $sql)) {
+            while ($row = mysqli_fetch_row($res)) {
+                $found = 1;
+                $alleles = $row[0];
+                $alleles_ary = explode("\t", $alleles);
+                foreach ($alleles_ary as $i => $v) {
+                    if ($v[0] == $v[1]) {
+                        $v = $v[0];
+                    } else {
+                        $v = $v[0] . "/" . $v[1];
+                    }
+                    $num_rows++;
+                    $marker_uid = $marker_index[$i];
+                    $dataList[] = array( "$marker_index[$i]", "$item", "$v");
+                }
+            }
+        } else {
+            dieNice("SQL", mysqli_error($mysqli));
+        }
+        if ($found == 0) {
+            dieNice("Error", "marker profile not found $item $sql");
+        }
+        $resultProfile[] = $item;
+    }
+} elseif (isset($_REQUEST['matrixDbId'])) {
+    $studyDbId = $_REQUEST['matrixDbId'];
+    $uniqueStr = chr(rand(65, 80)).chr(rand(65, 80)).chr(rand(65, 80)).chr(rand(65, 80));
+    $errorFile = "/tmp/tht/error_" . $uniqueStr . ".txt";
+    $cmd = "php allelematrix-offline.php \"$studyDbId\" \"$uniqueStr\" > /dev/null 2> $errorFile";
+    exec($cmd);
+    dieNice("asynchid", "$uniqueStr");
+} else {
+    //first query all data
+    dieNice("Error", "need markerprofileDbId");
+    $num_rows = 0;
+    $profile_list = array();
+    if ($currentPage == 0) {
+        $offset = 0;
+        $limit = $pageSize;
+    } else {
+        $offset = $currentPage * $pageSize;
+        $limit = $offset + $pageSize;
+    }
+    $sql = "select experiment_uid, line_record_uid, count from allele_byline_exp";
+    $res = mysqli_query($mysqli, $sql);
+    while ($row = mysqli_fetch_row($res)) {
+        $expid = $row[0];
+        $lineuid = $row[1];
+        $count = $row[2];
+        $item = $lineuid . "_" . $expid;
+        if (($num_rows == 0) && ($offset == 0)) {
+            $profile_list[] = $item;
+        } elseif ($num_rows > $offset) {
+            if (empty($profile_list)) {
+                $profile_list[] = $item;
+            } elseif ($num_rows < $limit) {
+                $profile_list[] = $item;
+            }
+        }
+        $num_rows += $count;
+    }
+    
+    foreach ($profile_list as $item) {
+        if (preg_match("/(\d+)_(\d+)/", $item, $match)) {
+            $lineuid = $match[1];
+            $expid = $match[2];
+        } else {
+            $results['metadata']['status'][] = array("code" => "parm", "message" => "Error: invalid format of marker profile id $item");
+            continue;
+        }
+
+        //now get just those selected
+        $sql = "select marker_index from allele_byline_expidx
+              where experiment_uid = $expid";
+        $res = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+        if ($row = mysqli_fetch_row($res)) {
+            $tmp = $row[0];
+            $marker_index = json_explode($tmp, true);
+            //$marker_index = explode(",", $tmp);
+        }
+        $sql = "select alleles from allele_byline_exp_ACTG
+              where line_record_uid = $lineuid
+              and experiment_uid = $expid";
+        $res = mysqli_query($mysqli, $sql);
+        if ($row = mysqli_fetch_row($res)) {
+            $tmp = $row[0];
+            $alleles = explode(",", $tmp);
+            foreach ($alleles as $key => $val) {
+                $dataList[$marker_index[$key]][] = $val;
+            }
+        }
+        $resultProfile[] = $item;
+    }
+}
+
+
+$tot_pag = ceil($num_rows / $pageSize);
+$pageList = array( "pageSize" => $pageSize, "currentPage" => $currentPage, "totalCount" => $num_rows, "totalPages" => $tot_pag );
+$results['metadata']['pagination'] = $pageList;
+$results['result']['data'] = $dataList;
+header("Content-Type: application/json");
+echo json_encode($results);
---
+++ brapi/v1.1/markers-search.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * supports GET and POST
+ **/
+
+require '../../includes/bootstrap.inc';
+$mysqli = connecti();
+
+$self = $_SERVER['PHP_SELF'];
+$script = $_SERVER["SCRIPT_NAME"]."/";
+$rest = str_replace($script, "", $self);
+$rest = explode("/", $rest);
+
+if (isset($rest[1]) && ($rest[1] == "table")) {
+    $outFormat = "table";
+} else {
+    $outFormat = "json";
+}
+$pageSize = 1000;
+$currentPage = 0;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $request = json_decode(file_get_contents('php://input'), true);
+    foreach ($request as $key => $val) {
+        if ($key == "markerDbIds") {
+            $markerDbIds = implode(",", $val);
+        } elseif ($key == "page") {
+            $currentPage = $val;
+        } elseif ($key == "pageSize") {
+            $pageSize = $val;
+        }
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['type'])) {
+        $type = $_GET['type'];
+    } else {
+        $type = "";
+    }
+    if (isset($_GET['name'])) {
+        $name = $_GET['name'];
+    } else {
+        $name = "";
+    }
+    if (isset($_GET['markerDbIds'])) {
+        $markerDbIds = $_GET['markerDbIds'];
+    } else {
+        $markerDbIds = "";
+    }
+    if (isset($_GET['pageSize'])) {
+        $pageSize = $_GET['pageSize'];
+    }
+    if (isset($_GET['page'])) {
+        $currentPage = $_GET['page'];
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    header("Content-Type: application/json");
+    die();
+} else {
+    dieNice("Error", "invalid request method");
+}
+
+function dieNice($msg)
+{
+    $linearray["metadata"]["pagination"] = null;
+    $linearray["metadata"]["status"] = array("code" => 1, "message" => "SQL Error: $msg");
+    $linearray["result"] = null;
+    $return = json_encode($linearray);
+    die("$return");
+}
+
+header("Content-Type: application/json");
+
+    $linearray['metadata']['pagination'] = $pageList;
+    $linearray['metadata']['status'] = array();
+    $linearray['metadata']['datafiles'] = array();
+
+    //first query all data
+    $sql = "select marker_uid, marker_name, marker_type_name from markers, marker_types
+        where markers.marker_type_uid = marker_types.marker_type_uid";
+    if ($type != "") {
+        $options = " and marker_type_name = \"$type\"";
+    } else {
+        $options = "";
+    }
+    if ($name != "") {
+        $options .= " and marker_name like \"$name\"";
+    }
+    if ($markerDbIds != "") {
+        $options .= " and marker_uid in ($markerDbIds)";
+    }
+    $sql .= $options;
+
+    $res = mysqli_query($mysqli, $sql) or dieNice(mysqli_error($mysqli));
+    $num_rows = mysqli_num_rows($res);
+    $tot_pag = ceil($num_rows / $pageSize);
+    $pageList = array( "pageSize" => $pageSize, "currentPage" => $currentPage, "totalCount" => $num_rows, "totalPages" => $tot_pag );
+    $linearray['metadata']['pagination'] = $pageList;
+
+    //now get just those selected
+    if ($currentPage == 0) {
+        $options = " limit $pageSize";
+    } else {
+        $offset = $currentPage * $pageSize;
+        if ($offset < 0) {
+            $offset = 0;
+        }
+        $options = " limit $offset, $pageSize";
+    }
+    $sql .= $options;
+    $res = mysqli_query($mysqli, $sql) or dieNice(mysqli_error($mysqli) . "<br>$sql<br>");
+    while ($row = mysqli_fetch_row($res)) {
+        $data["markerDbId"] = $row[0];
+        $data["defaultDisplayName"] = $row[1];
+        $data["type"] = $row[2];
+        $temp[] = $data;
+    }
+    $linearray['result']['data'] = $temp;
+    $return = json_encode($linearray);
+    echo "$return";
