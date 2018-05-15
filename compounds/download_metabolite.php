873549fa53b7ec8eadf135c5a39d7407e2f3a036
Clay Birkett <clb343@cornell.edu>
2018-05-15 11:00:19 -0700

    metabolite experiments - add page to display experiment and download trial

--- compounds/download_metabolite.php
+++ compounds/download_metabolite.php
@@ -4,7 +4,7 @@ namespace T3;
 
 require 'config.php';
 require_once $config['root_dir'].'includes/bootstrap.inc';
-require_once $config['root_dir'].'download_metabolite_class.php';
+require_once $config['root_dir'].'compounds/download_metabolite_class.php';
 
 // connect to database
 $mysqli = connecti();
