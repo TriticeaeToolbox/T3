522ed7215a070eb179c005da4eb0681e5ca6df7e
Clay Birkett <clb343@cornell.edu>
2018-05-15 10:58:18 -0700

    metabolite experiments - add page to display experiment and download trial

--- compounds/display_metabolite.js
+++ compounds/display_metabolite.js
@@ -2,11 +2,11 @@ function output_file(url) {
         window.open(url);
 }
 function output_file2(puid) {
-    url = "download_metabolite.php?function=downloadMean&pi=" + puid;
+    url = "compounds/download_metabolite.php?function=downloadMean&pi=" + puid;
     window.open(url);
 }
 
 function output_file_plot(puid) {
-    url = "download_metabolite.php?function=downloadPlot&trial_code=" + puid;
+    url = "compounds/download_metabolite.php?function=downloadPlot&trial_code=" + puid;
     window.open(url);
 }
