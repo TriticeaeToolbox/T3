14140edfbf0d4cc4dc208621b45644b18d127091
Clay Birkett <clb343@cornell.edu>
2019-04-16 07:12:13 -0700

    fix google analytics tracking

--- includes/analyticstracking.php
+++ includes/analyticstracking.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-39124275-3"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37631546-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-39124275-3');
+  gtag('config', 'UA-37631546-1');
 </script>
