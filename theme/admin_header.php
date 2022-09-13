04faa4a48b03973406c058c8aaed6e6b4775a770
The Triticeae Toolbox <clb343@cornell.edu>
2022-09-13 14:36:25 -0400

    update jQuery code.

--- theme/admin_header.php
+++ theme/admin_header.php
@@ -24,8 +24,8 @@
 <script type="text/javascript" src="theme/new.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js"></script>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<script src="//code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<script src="//code.jquery.com/jquery-3.6.1.min.js"></script>
+<script src="//code.jquery.com/ui/1.13.1/jquery-ui.min.js"></script>
 <script src="theme/jquery.smartmenus.min.js" type="text/javascript"></script>
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <!--script type="text/javascript" src="js/site.js"></script-->
